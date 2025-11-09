17c4b554a7e3fe7ba5e4c587c272114e0f550bfd
krauzerkrip <krauzerkrip@gmail.com>
2025-11-09 14:57:50 +0200

    fix(deploy): move from vcpkg to nixpkgs; add glad

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, ... }@inputs:
+
     let
       supportedSystems = [
         "x86_64-linux"
@@ -11,58 +13,56 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          f { inherit system pkgs; });
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs, system }: {
-        default = pkgs.mkShell {
-          # Uncomment to switch toolchain:
-          # stdenv = pkgs.clangStdenv;
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    cmake
+                    codespell
+                    conan
+                    cppcheck
+                    doxygen
+                    gtest
+                    lcov
+                    vcpkg
+                    vcpkg-tool
+                    ninja
+                    pkg-config
 
-          # Tools needed at configure/build time
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            cmake
-            clang-tools
-            codespell
-            cppcheck
-            doxygen
-            gtest
-            lcov
-            vcpkg
-            vcpkg-tool
-          ];
-
-          # Libraries available for compilation/linking
-          buildInputs = with pkgs; [
-            # OpenGL
-            libGL
-            libGLU
-            mesa
-
-            # X11 stack (for GLFW/Xwayland, etc.)
-            xorg.libX11
-            xorg.libXrandr
-            xorg.libXinerama
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXext
-            xorg.libXxf86vm
-            xorg.xorgproto
-
-            # Wayland stack
-            wayland
-            wayland-protocols
-            wayland-scanner
-            libxkbcommon
-          ] ++ (if system == "aarch64-darwin" then [ ] else [ pkgs.gdb ]);
-        };
-      });
+                    lua
+                    glfw
+                    assimp
+                    entt
+                    freetype
+                    glm
+                    imgui
+                    nlohmann_json
+                    pugixml
+                    luabridge
+                  ]
+                  ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+              };
+        }
+      );
     };
 }
